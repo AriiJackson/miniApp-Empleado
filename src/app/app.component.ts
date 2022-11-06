faa189b5a545254d198453cd2a484a23dbb51557
Arii Jackson <ariijackson@hotmail.com>
2022-11-06 20:05:35 -0300

    Subiendo archivos

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,47 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'appEmpleados';
+  title = 'Listado de Empleados';
+  employees: Empleado[] = [
+    new Empleado(
+      'Nuna',
+      'Jackson',
+      'Presidente',
+      100000
+    ),
+    new Empleado(
+      'Gina',
+      'Amaya',
+      'Directora',
+      90000
+    ),
+    new Empleado(
+      'Renata',
+      'Kathya',
+      'Empleado',
+      95000
+    ),
+    new Empleado(
+      'Caty',
+      'Amaya',
+      'Empleada',
+      80000
+    ),
+  ];
+
+  addEmployee() {
+    let employee = new Empleado(
+      this.inputName, 
+      this.inputLastName,
+      this.inputPosition,
+      this.inputSalary
+    );
+
+    this.employees.push(employee);
+  }
+
+  inputName: string = '';
+  inputLastName: string = '';
+  inputPosition: string = '';
+  inputSalary: number = 0;
 }
