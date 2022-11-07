55518bae9a754d5fce4419cac7b0d2a1f61f80d7
Arii Jackson <ariijackson@hotmail.com>
2022-11-06 21:38:33 -0300

    Componente Hijo - conexion entre componentes

---
+++ src/app/componente-hijo-empleado/componente-hijo-empleado.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+import { Empleado } from '../empleado.model';
+
+@Component({
+  selector: 'app-componente-hijo-empleado',
+  templateUrl: './componente-hijo-empleado.component.html',
+  styleUrls: ['./componente-hijo-empleado.component.css']
+})
+export class ComponenteHijoEmpleadoComponent implements OnInit {
+
+  // Conexion de componente padre a hijo 
+  @Input() e: Empleado;
+  @Input() i: number;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
