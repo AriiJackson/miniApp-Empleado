e3042397682244939727b86a209eb70f82bba541
Arii Jackson <ariijackson@hotmail.com>
2022-11-08 15:31:49 -0300

    Agregando mas ruteo, ruteo entre Home y Proyect

--- src/app/proyects-component/proyects-component.component.ts
+++ src/app/proyects-component/proyects-component.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Empleado } from '../empleado.model';
+import { EmpleadosService } from '../empleados.service';
+import { ServicioEmpleadoService } from '../servicio-empleado.service';
 
 @Component({
   selector: 'app-proyects-component',
@@ -7,9 +11,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProyectsComponentComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router, private myService: ServicioEmpleadoService, private employeeService: EmpleadosService) { 
   }
 
+  ngOnInit(): void {
+    this.employees = this.employeeService.employees;
+  }
+
+  backHome() {
+    this.router.navigate(['']);
+  }
+
+  employees: Empleado[] = [];
+
+  addEmployee() {
+    let employee = new Empleado(
+      this.inputName, 
+      this.inputLastName,
+      this.inputPosition,
+      this.inputSalary
+    );
+    //this.myService.showMessage(`Nombre del empleado: ${employee.name}`);
+    //this.employees.push(employee);
+    this.employeeService.addEmployeeService(employee);
+  }
+
+  inputName: string = '';
+  inputLastName: string = '';
+  inputPosition: string = '';
+  inputSalary: number = 0;
 }
